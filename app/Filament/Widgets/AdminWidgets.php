8df9490b8f2b5573159e3d3b5bc38cbd3667849c
Sohail Ahmed <sohail.comsians@gmail.com>
2024-03-01 02:14:32 +0500

    widgets

--- app/Filament/Widgets/AdminWidgets.php
+++ app/Filament/Widgets/AdminWidgets.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\Poet;
 use App\Models\Poetry;
 use App\Models\Comment;
+use App\Models\CulturalContent;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Filament\Widgets\StatsOverviewWidget\Card;
@@ -19,6 +20,8 @@ class AdminWidgets extends BaseWidget
             Stat::make('Users Registered Today', User::whereDate('created_at', today())->count()),
             Card::make('Total Poets', Poet::count()),
             Card::make('Total Poetry', Poetry::count()),
+            Card::make('Total Comments', Comment::count()),
+            Card::make('Total Cultural Content', CulturalContent::count()),
         ];
     }
 
